4996b45e03c3984eb933002d659802c3b297c38a
Hrishikesh Patil <hrishi0102business@gmail.com>
2024-05-30 16:28:33 +0530

    hls conversio

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { v4 as uuidv4 } from "uuid";
 import path from "path";
 import fs from "fs";
 import { exec } from "child_process";
+import { error } from "console";
+import { stderr, stdout } from "process";
 
 const app = express();
 
@@ -60,6 +62,22 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
   // command to convert video to HLS format using ffmpeg
   const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+
+  //use queue in production code
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    const videoUrl = `http://localhost:3000/uploads/courses/${lessonId}/index.m3u8`;
+    res.json({
+      message: "Video converted to HLS format",
+      videoUrl: videoUrl,
+      lessonId: lessonId,
+    });
+  });
 });
 
 app.listen(8000, () => {
