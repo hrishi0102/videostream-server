b39568a2a1f062c0bd7f214c8c625c7e670d03d4
Hrishikesh Patil <hrishi0102business@gmail.com>
2024-05-30 16:24:15 +0530

    upload endpoint

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
-import { v4 as uuisv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import path from "path";
+import fs from "fs";
+import { exec } from "child_process";
 
 const app = express();
+
+//multer (for handling files) middleware
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  },
+});
+//multer config
+const upload = multer({ storage: storage });
 
 // cors middleware
 app.use(
@@ -28,8 +42,24 @@ app.use(express.urlencoded({ extended: true }));
 //serve static files from a server
 app.use("/uploads", express.static("uploads"));
 
+//Routes
 app.get("/", (req, res) => {
   res.json({ message: "hello" });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const lessonId = uuidv4();
+  const videoPath = req.file.path;
+  const outputPath = `./uploads/courses/${lessonId}`;
+  const hlsPath = `${outputPath}/index.m3u8`;
+  console.log("hls path: ", hlsPath);
+
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+  }
+
+  // command to convert video to HLS format using ffmpeg
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
 });
 
 app.listen(8000, () => {
